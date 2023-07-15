d24a77f48f5ecd00f78bb43b714e55165bd7a8eb
Lucas Beleboni <beleboni.dev@gmail.com>
2023-07-15 19:45:41 -0300

    cors setup

---
+++ cyclic.config.js
@@ -0,0 +1,7 @@
+export const name = "lbb-api";
+export const script = "index.js";
+export const instances = 1;
+export const env = {
+    NODE_ENV: "production",
+    PORT: 3000, // A porta em que sua aplicação vai escutar (deve ser a mesma que você usa localmente)
+};
--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
+// Configurar o cabeçalho 'Access-Control-Allow-Origin' para permitir conexões de qualquer origem
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(json());
 app.use(cors());
 app.get("/", (req, res) => {
